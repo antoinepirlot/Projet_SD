d55d623ada95046483bae8677775308398e348a8
Jonathan Van de Vyver <jonathan.vandevyver@student.vinci.be>
2022-03-11 10:05:25 +0100

    .

--- src/Graph.java
+++ src/Graph.java
@@ -3,17 +3,36 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Graph {
 
-  public Graph(File aeroports, File vols) {
+  public List<String> aeroports = new ArrayList<>();
+  public List<String> vols = new ArrayList<>();
+
+  public Graph(File aeroportsFile, File volsFile) {
     try {
-      BufferedReader aeroportsBuffer = new BufferedReader(new FileReader(aeroports));
-      BufferedReader volsBuffer = new BufferedReader(new FileReader(vols));
-      String line = "";
-      while (line != null) {
-        line = aeroportsBuffer.readLine();
-        System.out.println(line);
+      BufferedReader aeroportsBuffer = new BufferedReader(new FileReader(aeroportsFile));
+      BufferedReader volsBuffer = new BufferedReader(new FileReader(volsFile));
+      String aeroportLine = "";
+      String volLine = "";
+      while (aeroportLine != null && volLine != null) {
+
+        volLine = volsBuffer.readLine();
+        if(aeroportLine != null){
+          aeroportLine = aeroportsBuffer.readLine();
+          if(aeroportLine != null) {
+            aeroports.add(aeroportLine);
+          }
+        }
+
+        if(volLine != null){
+          volLine = volsBuffer.readLine();
+          if(volLine != null){
+            vols.add(volLine);
+          }
+        }
       }
     } catch (FileNotFoundException e) {
       e.printStackTrace();
