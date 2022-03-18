1b36b45955e5eccab7b9a8a87c6411e70969d622
François Bardijn <francois.bardijn@hotmail.com>
2022-03-18 10:25:10 +0100

    [ADD] minimal distance algo

--- src/Graph.java
+++ src/Graph.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
@@ -60,6 +61,7 @@ public class Graph {
     } catch (IOException e) {
       e.printStackTrace();
     }
+    //calculerItineraireMiniminantDistance("NOU", "CHC");
   }
 
   public void calculerItineraireMinimisantNombreVol(String source, String destination) {
@@ -110,6 +112,58 @@ public class Graph {
   }
 
   public void calculerItineraireMiniminantDistance(String source, String destination) {
+    Aeroport aeroportSource = aeroports.get(source);
+    Aeroport aeroportDestination = aeroports.get(destination);
+    Map<Aeroport, Double> etiquettesProvisoires = new HashMap<Aeroport, Double>();
+    Map<Aeroport, Double> etiquettesDefinitives = new HashMap<Aeroport, Double>();
+    Map<Aeroport, Vol> sourceAeroport = new HashMap<>();
 
+    // Initialisation des etiquettes provisoires
+    List<Vol> listeVolsSource = volsSortantAeroport.get(aeroportSource);
+    for (Vol vol : listeVolsSource) {
+      Aeroport aeroportDesti = aeroports.get(vol.getIATADestination());
+      etiquettesProvisoires.put(aeroportDesti,
+          Util.distance(aeroportSource.getLatitude(), aeroportSource.getLongitude(),
+              aeroportDesti.getLatitude(), aeroportDesti.getLongitude()));
+
+      sourceAeroport.put(aeroportDesti, vol);
+    }
+
+    while (true) {
+      Aeroport aeroportDistanceMinimal = etiquettesProvisoires.keySet().stream()
+          .filter(a -> !etiquettesDefinitives.containsKey(a)).
+          min(Comparator.comparing(etiquettesProvisoires::get)).orElse(null);
+      if (aeroportDistanceMinimal == null) break;
+
+      Double distanceSourceDestination = etiquettesProvisoires.get(aeroportDistanceMinimal);
+
+      etiquettesDefinitives.put(aeroportDistanceMinimal, distanceSourceDestination);
+      if (aeroportDistanceMinimal.equals(aeroportDestination)) break;
+
+      List<Vol> listVolsAeroportDestination = volsSortantAeroport.get(aeroportDistanceMinimal);
+      if (listVolsAeroportDestination == null) continue;
+      for (Vol vol : listVolsAeroportDestination) {
+        Aeroport aeroportVol = aeroports.get(vol.getIATADestination());
+        Double distance =
+            distanceSourceDestination + Util.distance(aeroportDistanceMinimal.getLatitude(),
+                aeroportDistanceMinimal.getLongitude(), aeroportVol.getLatitude(),
+                aeroportVol.getLongitude());
+
+        Double ancienneDistance = etiquettesProvisoires.get(aeroportVol);
+        if (ancienneDistance == null || ancienneDistance > distance) {
+          etiquettesProvisoires.put(aeroportVol, distance);
+          sourceAeroport.put(aeroportVol, vol);
+        }
+      }
+    }
+
+    System.out.println(etiquettesDefinitives.get(aeroportDestination));
+    Vol vol = sourceAeroport.get(aeroportDestination);
+    while (vol != null) {
+      System.out.println(vol);
+      vol = sourceAeroport.get(aeroports.get(vol.getIATASource()));
+    }
+    System.out.println(vol);
+    System.out.println(sourceAeroport.get(aeroports.get(vol.getIATASource())));
   }
 }
